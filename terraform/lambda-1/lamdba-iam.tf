3345c37e2459cf298f19500d6d27a21a9e335be6
Aveek <aveeek.d22@gmail.com>
2022-03-07 13:56:57 +0100

    Modifed IAM resources.

--- terraform/lambda-1/lamdba-iam.tf
+++ terraform/lambda-1/lamdba-iam.tf
@@ -1,11 +1,11 @@
-resource "aws_iam_role_policy" "terraform_test_policy" {
-  name = "terraform_test_policy"
-  role = aws_iam_role.terraform_test_role.id
+resource "aws_iam_role_policy" "terraform_test_policy_lambda_1" {
+  name = "terraform_test_policy_lambda_1"
+  role = aws_iam_role.terraform_test_role_lambda_1.id
 
   policy = file("../terraform/lambda-1/iam-role/lambda-policy.json")
 }
 
-resource "aws_iam_role" "terraform_test_role" {
-  name = "terraform_test_role"
+resource "aws_iam_role" "terraform_test_role_lambda_1" {
+  name = "terraform_test_role_lambda_1"
   assume_role_policy = file("../terraform/lambda-1/iam-role/lambda-assume-policy.json")
 }
