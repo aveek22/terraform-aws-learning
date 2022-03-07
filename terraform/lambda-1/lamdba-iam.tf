944ee84f0f309305ef9dd89f540eabbcffdcd078
Aveek <aveeek.d22@gmail.com>
2022-03-07 13:51:29 +0100

    Structure changed

--- terraform/lambda-1/lamdba-iam.tf
+++ terraform/lambda-1/lamdba-iam.tf
@@ -2,10 +2,10 @@ resource "aws_iam_role_policy" "terraform_test_policy" {
   name = "terraform_test_policy"
   role = aws_iam_role.terraform_test_role.id
 
-  policy = file("iam-role/lambda-policy.json")
+  policy = file("../terraform/lambda-1/iam-role/lambda-policy.json")
 }
 
 resource "aws_iam_role" "terraform_test_role" {
   name = "terraform_test_role"
-  assume_role_policy = file("iam-role/lambda-assume-policy.json")
+  assume_role_policy = file("../terraform/lambda-1/iam-role/lambda-assume-policy.json")
 }
