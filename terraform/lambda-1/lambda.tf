65f397f8eb300244325fe59845aee6ff36738939
Aveek <aveeek.d22@gmail.com>
2022-03-07 13:59:04 +0100

    Modified IAM role

--- terraform/lambda-1/lambda.tf
+++ terraform/lambda-1/lambda.tf
@@ -11,7 +11,7 @@ data "archive_file" "terraform_lambda_function" {
 resource "aws_lambda_function" "terraform_lambda_function" {
   filename = local.lambda-zip-location
   function_name = "terraform_lambda_function"
-  role = aws_iam_role.terraform_test_role.arn
+  role = aws_iam_role.terraform_test_role_lambda_1.arn
   handler = "app.main"
 
   runtime = "python3.7"
